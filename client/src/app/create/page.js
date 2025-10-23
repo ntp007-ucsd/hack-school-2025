aa650bb69ae3e413991bfb635c86bdac17064767
Naam Pham <ntp007@ucsd.edu>
2025-10-22 18:47:45 -0700

    finished workshop 1

--- client/src/app/create/page.js
+++ client/src/app/create/page.js
@@ -8,6 +8,9 @@ import HomeButton from "@/components/HomeButton";
 export default function CreatePage() {
   // * Begin Activity 2a
   //TODO: Create a set of hooks for the title, description, and owner
+  const [title, setTitle] = useState ("")
+  const [descrition, setDescription] = useState ("")
+  const [owner, setOwner] = useState ("")
 
   // * End Activity
 
@@ -57,6 +60,36 @@ export default function CreatePage() {
          * One way of displaying it is:
          * Title: [         ]
          */}
+         <div>
+          <label htmlFor="titleInput">Title</label>
+          <input
+          id="titleInput"
+          className={styles.textInput}
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          />
+         </div>
+         <div className={styles.formRow}>
+          <label htmlFor='descriptionInput'>Description:</label>
+          <input
+          id="descriptionInput"
+          className={styles.textInput}
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          />
+         </div>
+         <div classNmae={styles.formRow}>
+          <label htmlFor="ownerInput">Owner:</label>
+          <input
+          id="ownerInput"
+          className={styles.textInput}
+          type="text"
+          value={owner}
+          onChange={(e) => setOwner(e.target.value)}
+          />
+         </div>
 
         {/* ACTIVITY END */}
 
---
+++ client/src/app/poll/[slug]/page.js
@@ -0,0 +1,4 @@
+export default function [slugs]({params}) {
+    const id=params.id;
+    return <h1>The page id: {id}</h1>
+}
